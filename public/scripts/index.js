934c456b19e9063f89843e21044df7b32a22f7fa
Atnatewoss <icodeyt21@gmail.com>
2025-02-08 09:59:14 -0800

    Separated the firebase config file
    Separated the firebase config file, and used ES6 modules to do so.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAuth,signInWithPopup, getRedirectResult, onAuthStateChanged, signInWithRedirect, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import {auth, db } from './firebaseConfig';
 
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyClF_kUmfMyrxDqP_iuu7BgoqqTtX67JRY",
-    authDomain: "valutracker-391c6.firebaseapp.com",
-    databaseURL: "https://valutracker-391c6-default-rtdb.firebaseio.com",
-    projectId: "valutracker-391c6",
-    storageBucket: "valutracker-391c6.firebasestorage.app",
-    messagingSenderId: "268267434732",
-    appId: "1:268267434732:web:49e075d9a0b22df4c4abc9",
-    measurementId: "G-X8RX5C4WKS"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// Initialize google provider
 const provider = new GoogleAuthProvider();
 
 // 🔹 Save User Info in Firestore
